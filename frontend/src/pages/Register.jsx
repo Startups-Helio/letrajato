37e0a87504c2dc6ff08a2a3d288ccd6dd4abab6b
Rafael Tolini <rftolini@gmail.com>
2025-04-30 15:46:16 -0300

    Fixing requirements

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,18 +1,11 @@
-<<<<<<< HEAD
-import RegisterForm from "../components/RegisterForm"
-
-function Register(){
-  return <RegisterForm route="/letrajato/user/register/" method="register"/>
-=======
-import Form from "../components/Form"
+import Form from "../components/RegisterForm"
 import NavBar from "../components/NavBar"
 
 function Register(){
   return <div>
           <NavBar/>
-          <Form route="/letrajato/user/register/" method="register"/>
+          <RegisterForm route="/letrajato/user/register/" method="register"/>
         </div>
->>>>>>> main
 }
 
 export default Register
