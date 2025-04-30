32459bbe2624ac1a952558d59b8030271aa1ac77
RafaelTolini <rftolini@gmail.com>
2025-04-30 14:29:59 -0300

    CNPJ verification when Registering (#3)
    A new component "RegisterForm" was created, which contains
    the new register form, including the CNPJ field. The submit
    button now sends an api request to the backend, which makes
    an api request to ReceitaWS, gets information regarding that
    CNPJ, and returns it to the frontend. Register is now
    temporarily not working.

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+import RegisterForm from "../components/RegisterForm"
+
+function Register(){
+  return <RegisterForm route="/letrajato/user/register/" method="register"/>
+=======
 import Form from "../components/Form"
 import NavBar from "../components/NavBar"
 
@@ -6,6 +12,7 @@ function Register(){
           <NavBar/>
           <Form route="/letrajato/user/register/" method="register"/>
         </div>
+>>>>>>> main
 }
 
 export default Register
