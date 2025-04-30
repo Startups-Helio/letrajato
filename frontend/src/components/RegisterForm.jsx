b911c5c919a793b163713992bd3875cdbbcd38b6
RafaelTolini <rftolini@gmail.com>
2025-04-30 14:29:59 -0300

    CNPJ verification when Registering (#3)
    A new component "RegisterForm" was created, which contains
    the new register form, including the CNPJ field. The submit
    button now sends an api request to the backend, which makes
    an api request to ReceitaWS, gets information regarding that
    CNPJ, and returns it to the frontend. Register is now
    temporarily not working.

---
+++ frontend/src/components/RegisterForm.jsx
@@ -0,0 +1,65 @@
+import {useState} from "react"
+import api from "../api"
+import {useNavigate} from "react-router-dom"
+import {ACCESS_TOKEN, REFRESH_TOKEN} from "../constants"
+import "../styles/Form.css"
+import LoadingIndicator from "./LoadingIndicator"
+
+function RegisterForm({route, method}){
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [cnpj, setCnpj] = useState("")
+  const [loading, setLoading] = useState(false)
+  const [consulta, setConsulta] = useState(null)
+  const navigate = useNavigate()
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    try {
+      api
+      .get(`/letrajato/cnpj/${cnpj}/`)
+      .then((res) => res.data)
+      .then((data) => {setConsulta(data); alert(data.nome)})
+      .catch((err) => alert(err));
+    } catch (error) {
+      alert(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form-container">
+      <h1>Register</h1> {/* Replace {name} with a valid string */}
+      <input
+        className="form-input"
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        className="form-input"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <input
+        className="form-input"
+        type="text"
+        value={cnpj}
+        onChange={(e) => setCnpj(e.target.value)}
+        placeholder="CNPJ"
+      />
+      {loading && <LoadingIndicator />}
+      <button className="form-button" type="submit">
+        Submit
+      </button> {/* Replace {name} with a valid string */}
+    </form>
+  );
+}
+
+export default RegisterForm
