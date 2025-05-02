2b66c8dad69309da6a6d3471221dc875e2b9da8b
Rafael Tolini <rftolini@gmail.com>
2025-05-02 18:11:25 -0300

    Admin route

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("letrajato/admin/", admin.site.urls),
     path("letrajato/user/register/", CreateUserView.as_view(), name="register"),
     path("letrajato/token/", TokenObtainPairView.as_view(), name="get_token"),
     path("letrajato/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
@@ -21,3 +21,4 @@ if settings.DEBUG:
 else:
     # For production, you might still want to serve media files
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
