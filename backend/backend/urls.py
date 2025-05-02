3c4ac5a8a24fe624df9fd10742e999673e2c1bb6
Rafael Tolini <rftolini@gmail.com>
2025-05-02 16:52:23 -0300

    tentando

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -5,10 +5,6 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("letrajato/user/register/", CreateUserView.as_view(), name="register"),
@@ -17,3 +13,11 @@ urlpatterns = [
     path("letrajato-auth/", include("rest_framework.urls")),
     path("letrajato/", include("letrajato.urls")),
 ]
+
+# Then add static/media URLs
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+else:
+    # For production, you might still want to serve media files
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
