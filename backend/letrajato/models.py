04441b032e607f4e0074af01331566432e5aa2bc
Ubuntu <ubuntu@ip-172-31-81-251.ec2.internal>
2025-05-05 20:12:27 +0000

    fix

--- backend/letrajato/models.py
+++ backend/letrajato/models.py
@@ -26,7 +26,11 @@ class CustomUser(AbstractUser):
     nome_empresa = models.CharField(max_length=255)
     verificado = models.BooleanField(default=False)
     verification_token = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
-    
+    def save(self, *args, **kwargs):
+        if not self.verification_token:
+            self.verification_token = uuid.uuid4()
+        super().save(*args, **kwargs)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'cnpj', 'nome_empresa']
     
