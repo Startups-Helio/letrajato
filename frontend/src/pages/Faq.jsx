64536bbf490a81028d161e3cbd381c0276b0d73b
Luiz Raffaini <eduardoraffaini@192.168.68.110>
2025-05-11 12:23:23 -0300

    adicionando pagina faq e concertando not-found page

---
+++ frontend/src/pages/Faq.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import "../styles/Faq.css";
+import NavBar from '../components/NavBar';
+import { Link } from 'react-router-dom';
+
+function Faq() {
+  return (
+    <div className="suport-container">
+        <NavBar />
+        <section className="gray-section">
+            <div className="gray-text">
+                <h1>FAQs</h1>
+                <p>
+                Alguma pergunta? Aqui você encontrará as respostas mais valorizadas pelos nossos parceiros, além de instruções passo a passo e suporte.
+                </p>
+            </div>
+            <div className="gray-image">
+                <img src="src/assets/suporte_image.png" loading="lazy" />
+            </div>
+        </section>
+        <section className="white-section">
+            <div className="inner">
+                <div className="white-links">
+                    <Link to="/*">FAQs</Link>
+                    <Link to="/*">About us</Link>
+                    <Link to="/*">Ticket Suport</Link>
+                    <Link to="/*">Enter in contact</Link>
+                    <Link to="/*">Become verified</Link>
+                </div>
+                <div className="white-questions">
+                    
+                </div>
+            </div>
+        </section>
+    </div>
+  );
+}
+
+export default Faq;
--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,17 @@
+import NavBar from '../components/NavBar';
+import "../styles/NotFound.css";
+import React from 'react';
+
 function NotFound(){
-  return <div>
-    <h1>404 Not Found</h1>
-    <p>A página que você procurou não existe!</p>
-  </div>
+  return (
+    <div className="not-found-page">
+      <NavBar />
+      <div className="not-found-container">
+        <h1>404 Not Found</h1>
+        <p>A página que você procurou não existe ou esta em desenvolvimento!</p>
+      </div>
+    </div>
+  )
 }
 
 export default NotFound
